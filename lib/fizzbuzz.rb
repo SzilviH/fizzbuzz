065a67aaeccbb070add56cb6959acf22ca28749a
Katie Cornish <katielcornish@gmail.com>
2018-12-18 12:03:56 +0000

    improve fizzbuzz code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,9 @@
 def fizzbuzz(number)
-  if number == 3
-    'fizz'
-  elsif number == 5
+  if number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
+  elsif number % 5 == 0
     'buzz'
-  end 
+  elsif number % 3 == 0
+    'fizz'
+  end
 end
