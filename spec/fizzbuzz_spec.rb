01e395a0b8f5898fa982a0af99d4848152874a66
Szilvi Horvath <szil.horvath@gmail.com>
2018-12-18 11:24:53 +0000

    pass first rspec test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,7 @@
 require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when when passed 3' do
-    expect (fizzbuzz(3)).to eq 'fizz'
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
   end
 end
