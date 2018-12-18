94e84859ee0dbeec19cc4559c4b88418198feaef
Szilvi Horvath <szil.horvath@gmail.com>
2018-12-18 12:11:35 +0000

    all tests passed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -14,8 +14,11 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
 
-  it 'returns "buzz" when passed 6' do
+  it 'returns "fizz" when passed 6' do
     expect(fizzbuzz(6)).to eq 'fizz'
   end
 
+  it 'returns 2 when passed 2' do
+    expect(fizzbuzz(2)).to eq '2'
+  end
 end
